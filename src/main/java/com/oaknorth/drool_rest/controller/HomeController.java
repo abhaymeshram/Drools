81eb5af6850e615a10f3fd6db48bf4615a4cf8e4
asus <asus@Abhay>
2017-05-03 22:36:01 +0530

    Removed unused code

--- src/main/java/com/oaknorth/drool_rest/controller/HomeController.java
+++ src/main/java/com/oaknorth/drool_rest/controller/HomeController.java
@@ -9,7 +9,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.oaknorth.drool_rest.models.Employee;
 import com.oaknorth.drool_rest.services.RulesService;
 
 @Controller
@@ -18,20 +17,8 @@ public class HomeController {
 	@Autowired
 	RulesService ruleService;
 
-	@RequestMapping(value="/home")
+	@RequestMapping(value="/")
 	public ModelAndView test(HttpServletResponse response) throws IOException{
-		Employee e1 = new Employee();
-    	e1.setFirstName("Test");
-    	e1.setLastName("Test");
-    	e1.setAge(30);
-    	e1.setExperience(3.3);
-    	e1.setId(1);
-    	
-    	System.out.println("Employee before Rules fired ! " + e1);
-    	
-    	Employee e2 = ruleService.applyRules(e1);
-    	
-    	System.out.println("Employee after Rules fired ! " + e2);
 		return new ModelAndView("home");
 	}
 }
